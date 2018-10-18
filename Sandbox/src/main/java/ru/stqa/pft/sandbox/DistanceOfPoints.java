4e48ec81ebe00753469402bcf3bc4cb8bde8f4ed
Aksenov Andrey <a.aksenov@lcgroup.su>
2018-10-18 20:00:14 +0300

    выполнил второе задание

--- Sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceOfPoints.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/DistanceOfPoints.java
@@ -1,4 +1,11 @@
 package ru.stqa.pft.sandbox;
 
 public class DistanceOfPoints {
+
+    public static void main(String[] args) {
+        Point p1 = new Point(2.0,3.0);
+        Point p2 = new Point (5.0,4.0);
+        System.out.printf("расстояние между двумя точками равно " + "%.2f", p1.distance(p2));
+    }
+
 }
--- Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ Sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,4 +1,17 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
+    double x;
+    double y;
+
+    public Point (double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p){
+        double operand1 = Math.pow((p.x - this.x),2);
+        double operand2 = Math.pow((p.y - this.y),2);
+        return Math.sqrt(operand1 + operand2);
+    }
 }
