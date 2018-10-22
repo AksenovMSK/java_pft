30563cacfaee5207b813ea704ce83e646e2d5f6c
Aksenov Andrey <a.aksenov@lcgroup.su>
2018-10-22 14:35:26 +0300

    убрал лишний импорт после рефакторинга

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupDeletionTests.java
@@ -1,10 +1,6 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.concurrent.TimeUnit;
-import org.testng.annotations.*;
-import static org.testng.Assert.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
 
