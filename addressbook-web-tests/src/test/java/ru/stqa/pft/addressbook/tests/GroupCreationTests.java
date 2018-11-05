f3d89d7029f15fd44a0e92d9c0357765da288440
AksenovMSK <a.aksenov@lcgroup.su>
2018-11-05 16:53:41 +0300

    заменил метод поиска максимального элемента на лямда выражение

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -22,14 +23,31 @@ public class GroupCreationTests extends TestBase {
     Assert.assertEquals(after.size(), before.size() + 1);
 
     // ищем в новом списке групп, группу с максимальным идентификатором ID
-    int max = 0;
-    for (GroupData g : after){
-      if(g.getId() > max){
-        max = g.getId();
-      }
-    }
+
+    // способ №1
+//    int max = 0;
+//    for (GroupData g : after){
+//      if(g.getId() > max){
+//        max = g.getId();
+//      }
+//    }
+
+    // способ №2
+//    Comparator<? super GroupData> ById = new Comparator<GroupData>() {
+//      @Override
+//      public int compare(GroupData o1, GroupData o2) {
+//        return Integer.compare(o1.getId(),o2.getId());
+//      }
+//    };
+//    int max = after.stream().max(ById).get().getId();
+
+    // способ №3 заменяем компоратор с переопределённым методом на лямда выражение (анонимную функцию) и вставляем в метод маx()
+    // - список превращаем в поток
+    // - по этому потоку пробегается функция сравниватель, находит максимальный элемент
+//  int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
+
     // записываем найденый идентификатор к добавленной группе (в момент создания мы его не знали)
-    group.setId(max);
+    group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
     //добавляем обновлённую группу к изначальному списку для сравнения
     before.add(group);
     Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
