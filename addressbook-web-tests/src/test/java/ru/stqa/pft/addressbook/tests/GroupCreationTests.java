864b5dd528f95281453c2bb42a4b5f66c1382a9e
AksenovMSK <44177189+AksenovMSK@users.noreply.github.com>
2018-11-11 22:30:46 +0300

    убрал сортировку из удаления группы, список заменил на множество

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -4,53 +4,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
-import java.util.List;
+import java.util.Set;
 
 public class GroupCreationTests extends TestBase {
 
   @Test
   public void testGroupCreation() {
     app.goTo().groupPage();
-    List<GroupData> before = app.group().list();
+    Set<GroupData> before = app.group().all();
     GroupData group = new GroupData().withName("test1").withHeader("test2").withFooter("test3");
     app.group().create(group);
-    List<GroupData> after = app.group().list();
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size() + 1);
 
+    // преобразуем множество в поток, а затем поток объектов GroupData превращаем в поток чисел, выбираем максимальное значение
+    group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
     before.add(group);
-    Comparator<? super GroupData> ById = (g1, g2) -> Integer.compare(g1.getId(),g2.getId());
-    before.sort(ById);
-    after.sort(ById);
     Assert.assertEquals(before,after);
-
-    // ищем в новом списке групп, группу с максимальным идентификатором ID
-
-    // способ №1
-//    int max = 0;
-//    for (GroupData g : after){
-//      if(g.getId() > max){
-//        max = g.getId();
-//      }
-//    }
-
-    // способ №2
-//    Comparator<? super GroupData> ById = new Comparator<GroupData>() {
-//      @Override
-//      public int compare(GroupData o1, GroupData o2) {
-//        return Integer.compare(o1.getId(),o2.getId());
-//      }
-//    };
-//    int max = after.stream().max(ById).get().getId();
-
-    // способ №3 заменяем компоратор с переопределённым методом на лямда выражение (анонимную функцию) и вставляем в метод маx()
-    // - список превращаем в поток
-    // - по этому потоку пробегается функция сравниватель, находит максимальный элемент
-//  int max = after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId();
-
-    // записываем найденый идентификатор к добавленной группе (в момент создания мы его не знали)
-    //group.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(),o2.getId())).get().getId());
-    //добавляем обновлённую группу к изначальному списку для сравнения
   }
 
 }
