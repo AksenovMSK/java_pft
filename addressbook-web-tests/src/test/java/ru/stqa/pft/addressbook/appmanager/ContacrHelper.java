9cc12bba5718b66a9844c64e661a391d3fc18d34
AksenovMSK <44177189+AksenovMSK@users.noreply.github.com>
2018-11-18 21:04:30 +0300

    изменил модель контакта и вспомогательный класс для сравнения адреса и почты

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContacrHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ContacrHelper.java
@@ -96,11 +96,15 @@ public class ContacrHelper extends BaseHelper {
             int id = Integer.parseInt(cells.get(0).findElement(By.tagName("input")).getAttribute("value"));
             String lastName = cells.get(1).getText();
             String firstName = cells.get(2).getText();
+            String address = cells.get(2).getText();
+            String allEmails = cells.get(2).getText();
             String allPhones = cells.get(5).getText();
             contactCach.add(new ContactData()
                     .withId(id)
                     .withFirstname(firstName)
                     .withLastname(lastName)
+                    .withAddress(address)
+                    .withAllEmail(allEmails)
                     .withAllPhones(allPhones));
         }
         return new Contacts(contactCach);
@@ -128,16 +132,24 @@ public class ContacrHelper extends BaseHelper {
         initContactModification(contact.getId());
         String firstname = wd.findElement(By.name("firstname")).getAttribute("value");
         String lastname = wd.findElement(By.name("lastname")).getAttribute("value");
+        String address = wd.findElement(By.name("address")).getText();
         String home = wd.findElement(By.name("home")).getAttribute("value");
         String mobile = wd.findElement(By.name("mobile")).getAttribute("value");
         String work = wd.findElement(By.name("work")).getAttribute("value");
+        String email = wd.findElement(By.name("email")).getAttribute("value");
+        String email2 = wd.findElement(By.name("email2")).getAttribute("value");
+        String email3 = wd.findElement(By.name("email3")).getAttribute("value");
         wd.navigate().back();
         return new ContactData()
                 .withId(contact.getId())
                 .withFirstname(firstname)
                 .withLastname(lastname)
+                .withAddress(address)
                 .withHomePhone(home)
                 .withMobilePhone(mobile)
-                .withWorkPhone(work);
+                .withWorkPhone(work)
+                .withEmail(email)
+                .withEmail2(email2)
+                .withEmail3(email3);
     }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -12,6 +12,9 @@ public class ContactData {
     private String workPhone;
     private String allPhones;
     private String email;
+    private String email2;
+    private String email3;
+    private String allEmail;
     private String group;
 
     public ContactData withId(int id) {
@@ -59,10 +62,27 @@ public class ContactData {
         return this;
     }
 
+    public ContactData withEmail2(String email2) {
+        this.email2 = email2;
+        return this;
+    }
+
+    public ContactData withEmail3(String email3) {
+        this.email3 = email3;
+        return this;
+    }
+
+    public ContactData withAllEmail(String allEmail) {
+        this.allEmail = allEmail;
+        return this;
+    }
+
     public ContactData withGroup(String group) {
         this.group = group;
         return this;
     }
+
+    public int getId() { return id; }
 
     public String getFirstname() {
         return firstname;
@@ -88,18 +108,20 @@ public class ContactData {
         return workPhone;
     }
 
-    public String getAllPhones() { return allPhones;
-    }
+    public String getAllPhones() { return allPhones; }
 
     public String getEmail() {
         return email;
     }
 
-    public String getGroup() { return group;
-    }
+    public String getEmail2() { return email2; }
 
-    public int getId() { return id;
-    }
+    public String getEmail3() { return email3; }
+
+    public String getAllEmail() { return allEmail; }
+
+    public String getGroup() { return group; }
+
 
     @Override
     public String toString() {
@@ -124,5 +146,4 @@ public class ContactData {
     public int hashCode() {
         return Objects.hash(id, firstname, lastname);
     }
-
 }
