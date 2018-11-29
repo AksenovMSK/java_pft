7cf6b780e975a41fa7c4cea20718543e0951ba38
AksenovMSK <44177189+AksenovMSK@users.noreply.github.com>
2018-11-29 16:14:12 +0300

    увеличил количество полей для сравнения у контактов до 5
    пока без "скрытых" полей

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -200,12 +200,14 @@ public class ContactData {
         ContactData that = (ContactData) o;
         return id == that.id &&
                 Objects.equals(firstName, that.firstName) &&
-                Objects.equals(lastName, that.lastName);
+                Objects.equals(lastName, that.lastName) &&
+                Objects.equals(address, that.address) &&
+                Objects.equals(mobilePhone, that.mobilePhone) &&
+                Objects.equals(email, that.email);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, firstName, lastName);
+        return Objects.hash(id, firstName, lastName, address, mobilePhone, email);
     }
-
 }
