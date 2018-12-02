e183f59d3059b17a69ef160a1fd0bc29434196bc
AksenovMSK <44177189+AksenovMSK@users.noreply.github.com>
2018-12-02 11:56:32 +0300

    подправил тесты, проблемы с конфигами продолжаются

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTests.java
@@ -4,6 +4,10 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.File;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -12,14 +16,20 @@ public class ContactDeletionTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
+        if(app.db().groups().size() == 0){
+            app.goTo().groupPage();
+           app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
+        }
         if(app.db().contacts().size() == 0){
+            Groups groups = app.db().groups();
             ContactData contact = new ContactData()
                     .withFirstName("Nametest")
                     .withLastName("Lastnametest")
                     .withAddress("Test home 123")
                     .withMobilePhone("111111111")
                     .withEmail("test@mail.ru");
-            app.contact().create(contact, true);
+            File photo = new File("src/test/resources/stru.png");
+            app.contact().create(contact.withPhoto(photo).inGroup(groups.iterator().next()), true);
             app.goTo().homePage();
         }
     }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -4,6 +4,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.File;
 
@@ -14,16 +16,20 @@ public class ContactModificationTests extends TestBase {
 
     @BeforeMethod
     public void ensurePreconditions(){
+        if(app.db().groups().size() == 0){
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("test1").withHeader("test2").withFooter("test3"));
+        }
         if(app.db().contacts().size() == 0){
+            Groups groups = app.db().groups();
             ContactData contact = new ContactData()
                     .withFirstName("Nametest")
                     .withLastName("Lastnametest")
                     .withAddress("Test home 123")
                     .withMobilePhone("111111111")
-                    .withFax("222222222")
-                    .withEmail("test@mail.ru")
-                    .withHomepage("testhomepage.ru");
-            app.contact().create(contact, true);
+                    .withEmail("test@mail.ru");
+            File photo = new File("src/test/resources/stru.png");
+            app.contact().create(contact.withPhoto(photo).inGroup(groups.iterator().next()), true);
             app.goTo().homePage();
         }
     }
