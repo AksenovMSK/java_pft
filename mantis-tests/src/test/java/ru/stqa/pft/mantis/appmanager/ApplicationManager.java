d2c4330332a4c0304cf77c7874df4e9202b1517a
AksenovMSK <44177189+AksenovMSK@users.noreply.github.com>
2018-12-07 15:08:29 +0300

    добавил класс базового помошника и библиотеку для почтового сервера

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -29,8 +29,6 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-
   }
 
   public void stop() {
